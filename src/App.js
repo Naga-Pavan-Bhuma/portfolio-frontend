e72e7a8c0aaf2cfcd048bbbbe07c2020cdd548e5
B. Naga Pavan <bhumanagapavan@gmail.com>
2025-06-11 10:45:49 +0530

    Small Change

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import { useEffect } from "react";
 export default function App() {
+  useEffect(() => {
+    document.documentElement.classList.add("dark");
+  }, []);
   return (
     <>
       <Header />
